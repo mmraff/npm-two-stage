a87bb8bb69f72dc94f27a1d25486658712e2c0e8
Matthew Rafferty <mmraff@gmail.com>
2022-04-25 15:52:58 -0600

    add 'istanbul ignore's

--- test/fixtures/self-mocks/lib/constants.js
+++ test/fixtures/self-mocks/lib/constants.js
@@ -1,3 +1,4 @@
+/* istanbul ignore file */
 /*
   The only reason to mock this module (so far) is to make targetVersion mutable.
   The path of the original must be relative to the target location, which will be
