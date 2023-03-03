4cf635d8c664706a0f41c3ac25e4d2ff537ff905
Matthew Rafferty <mmraff@gmail.com>
2023-03-03 16:17:51 -0700

    cleanup

--- src/download/read-from-tarball.js
+++ src/download/read-from-tarball.js
@@ -2,37 +2,6 @@ const fs = require('fs')
 
 const tar = require('tar')
 
-/*
-  TODO:
-  * create a package directory
-  * add in this order: yarn.lock, package-lock.json, npm-shrinkwrap.json
-  * make a tarball, use as a test case
-
-  * for a full-fledged test, here is an example package to install (because
-    the numbers are right for it in the registry-mocks/content):
-      npm i --save-exact once@1.3.2 --before 2016
-    That has a single dep, wrappy@1; the --before will ensure we get v1.0.1
-    of the dep instead of the latest.
-    Get yarn, and use it to generate a yarn.lock.
-    Then we can delete node_modules, generate npm-shrinkwrap.json, and turn it
-    into a package (tarball).
-*/
-
-/*
-  stream.Readable:
-  * events of interest: end, error, close
-  * pipe() "returns a reference to the destination stream". <-- nodejs api doc.
-  * default behavior when piped is that the destination ends when the source
-    emits end; that's why I was trying emit 'end' from the readStream (but was
-    unintentionally emitting it from the tarParser).
-  * readStream 'close' emits after destroy() is called on it.
-  tar.Parse:
-  * "All the standard writable stream stuff is supported." <-- tar v6 api doc.
-  * Does *not* have a destroy() method (probably because it was written before
-    that feature was added to Writable).
-    (does not have a resume() method, nor should it)
-*/
-
 // Determined by creating a tar file, using --delete to remove the content,
 // gzipping it, then taking the size of the result:
 // If file given as tarball is not at least this size, it can't possibly be a
@@ -44,7 +13,7 @@ const MIN_TARBALL_SIZE = 57
 module.exports = (tarball, priorityList) => new Promise((resolve, reject) => {
   const arg1Msg = 'First argument must be path to a tarball'
   const arg2Msg = 'Second argument must be an array of one or more filepaths'
-  if (!tarball)
+  if (tarball === undefined || tarball === null || tarball === '')
     return reject(new SyntaxError(arg1Msg))
   if (typeof tarball !== 'string')
     return reject(new TypeError(arg1Msg))
@@ -111,9 +80,6 @@ module.exports = (tarball, priorityList) => new Promise((resolve, reject) => {
     tarParser.removeListener('entry', processEntry)
     error = err
   })
-  // As far as I've seen, the 'close' event is always preceded by 'finish',
-  // when there is a 'finish' event
-  //.once('finish', () => console.log('tarParser finished.'))
   .once('close', function() {
     if (!content && !error) {
       error = new Error(`Target${priorityList.length > 1 ? 's' : ''} not found`)
